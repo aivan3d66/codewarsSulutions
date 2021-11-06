c21e26107f8ff317f1dd9fba70cc591d4c982325
Ivan <adamovskij00@mail.ru>
2021-11-06 16:22:21 +0300

    add (function with Default Parameters)

--- main.js
+++ main.js
@@ -46,3 +46,7 @@ const magic = () => new Date();
 //  Write Arrow Functions with Parameters
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+//  Set Default Parameters for Your Functions
+const increment = (number, value = 1) => number + value;
+console.log(increment(5,2));
