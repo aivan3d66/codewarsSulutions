6257c770942fadc89023d289d7a2bbf1ac77f7ff
Ivan <adamovskij00@mail.ru>
2021-11-07 09:45:42 +0300

    add (Destructuring Assignment to Assign Variables from Objects)

--- main.js
+++ main.js
@@ -73,3 +73,13 @@ const HIGH_TEMPERATURES = {
 
 const {today, tomorrow} = HIGH_TEMPERATURES;
 console.log(today, tomorrow);
+
+//  Use Destructuring Assignment to Assign Variables from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+// Only change code below this line
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+console.log(highToday, highTomorrow);
