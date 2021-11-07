2740bc3947914f12170dbfeeccc3f3c8939b9872
Ivan <adamovskij00@mail.ru>
2021-11-07 09:50:21 +0300

    add (Use Destructuring Assignment to Pass an Object as a Function's Parameters)

--- main.js
+++ main.js
@@ -107,3 +107,16 @@ function removeFirstTwo(list) {
 }
 const arr = removeFirstTwo(source);
 console.log(arr);
+
+//  Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+const half = ({max, min}) => (max + min) / 2.0;
+console.log(half(stats));
