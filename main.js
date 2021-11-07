44bdf310fc74853d035d28e277465ab0a1878871
Ivan <adamovskij00@mail.ru>
2021-11-07 09:44:05 +0300

    add destructuring to obj

--- main.js
+++ main.js
@@ -63,3 +63,13 @@ let arr2;
 
 arr2 = [...arr1];
 console.log(arr2);
+
+//  Use Destructuring Assignment to Extract Values from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+console.log(today, tomorrow);
