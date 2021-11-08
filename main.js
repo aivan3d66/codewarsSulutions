0d8d7a92602f84cdca635321e9d75477f0636542
Ivan <adamovskij00@mail.ru>
2021-11-08 18:40:45 +0300

    Combine Arrays with the Spread Operator

--- main.js
+++ main.js
@@ -202,3 +202,12 @@ function copyMachine(arr, num) {
 }
 
 console.log(copyMachine([true, false, true], 2));
+
+//  Combine Arrays with the Spread Operator
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+    return sentence;
+}
+
+console.log(spreadOut());
