5776bd2d2bfb6448c937931164df10931c9582ac
Ivan <adamovskij00@mail.ru>
2021-11-08 15:34:21 +0300

    Copy Array Items Using slice()

--- main.js
+++ main.js
@@ -178,3 +178,13 @@ function htmlColorNames(arr) {
 }
 
 console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+
+//  Copy Array Items Using slice()
+function forecast(arr) {
+    // Only change code below this line
+    return arr.slice(2,4);;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
