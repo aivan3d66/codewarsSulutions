2dc378c0f619b3e311b994ece653cbb68080623a
Ivan <adamovskij00@mail.ru>
2021-11-08 18:37:53 +0300

    Copy an Array with the Spread Operator

--- main.js
+++ main.js
@@ -186,5 +186,19 @@ function forecast(arr) {
     return arr.slice(2,4);;
 }
 
-// Only change code above this line
+//  Only change code above this line
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+//  Copy an Array with the Spread Operator
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // Only change code below this line
+        newArr.push([...arr]);
+        // Only change code above this line
+        num--;
+    }
+    return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
