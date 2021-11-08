b0eed5c8a12941ed640075a7972629c04011458a
Ivan <adamovskij00@mail.ru>
2021-11-08 18:45:41 +0300

    Check For The Presence of an Element With indexOf()

--- main.js
+++ main.js
@@ -211,3 +211,16 @@ function spreadOut() {
 }
 
 console.log(spreadOut());
+
+//  Check For The Presence of an Element With indexOf()
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    if (arr.indexOf(elem) >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+    // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
