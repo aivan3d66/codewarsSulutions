f57ab9b4f83b1a4a5697b7c97a819694c98b838b
Ivan <adamovskij00@mail.ru>
2021-11-08 19:12:47 +0300

    Access Property Names with Bracket Notation

--- main.js
+++ main.js
@@ -287,3 +287,22 @@ userActivity.data.online = 45;
 // Only change code above this line
 
 console.log(userActivity);
+
+//  Access Property Names with Bracket Notation
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+    // Only change code below this line
+    let inventory = foods[scannedItem];
+    return inventory;
+    // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
