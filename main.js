0c7c81bc1f527320846534a453cebd50ee38a776
Ivan <adamovskij00@mail.ru>
2021-11-08 15:13:43 +0300

    Add Items to an Array with push() and unshift

--- main.js
+++ main.js
@@ -141,3 +141,14 @@ const createPerson = (name, age, gender) => {
     return {name,age,gender};
 };
 console.log(createPerson);
+
+//  Add Items to an Array with push() and unshift()
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
