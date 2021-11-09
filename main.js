9a59f16ebcf471cee2ff2e1deff1058e5c46dd76
Ivan <adamovskij00@mail.ru>
2021-11-09 21:12:45 +0300

    Repeat a String Repeat a String

--- main.js
+++ main.js
@@ -488,3 +488,23 @@ function confirmEnding(str, target) {
 }
 
 confirmEnding("Bastian", "n");
+
+//  Repeat a String Repeat a String
+function repeatStringNumTimes(str, num) {
+    // repeat after me
+    let newStr = '';
+
+    // with a 'for' loop
+    //   for (i = 0; i < num; i++) {
+    //     newStr += str;
+    //   }
+
+    // with a 'while' loop
+    while (num > 0) {
+        newStr += str;
+        num--;
+    }
+    return newStr;
+}
+
+repeatStringNumTimes("abc", 3);
