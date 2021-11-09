e89ce9837545a85a2d374ee287fe83a9fcc71da4
Ivan <adamovskij00@mail.ru>
2021-11-09 21:00:23 +0300

    Convert Celsius to Fahrenheit

--- main.js
+++ main.js
@@ -419,3 +419,11 @@ function addFriend(userObj, friend) {
 }
 
 console.log(addFriend(user, 'Pete'));
+
+//  Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+    let fahrenheit = celsius * 9 / 5 + 32;
+    return fahrenheit;
+}
+
+convertToF(30);
