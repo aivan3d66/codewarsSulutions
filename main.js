174310e7254d4ce388d3af88bacab869051c648d
Ivan <adamovskij00@mail.ru>
2021-11-09 21:20:18 +0300

    add spyFunc

--- main.js
+++ main.js
@@ -508,3 +508,25 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+// spyOn
+function spyOn (func) {
+    var callVals = [], returnVals = [], callCount = 0;
+    function spy () {
+        ++callCount;
+        callVals = [].concat.apply(callVals, arguments);
+        var val = func.apply(this, arguments);
+        returnVals.push(val);
+        return val;
+    };
+    spy.callCount = function () {
+        return callCount;
+    };
+    spy.wasCalledWith = function (val) {
+        return callVals.indexOf(val) > -1;
+    };
+    spy.returned = function (val) {
+        return returnVals.indexOf(val) > -1;
+    };
+    return spy;
+};
