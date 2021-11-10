d66a749aa72c6448ea568304487010f720f2a46a
Ivan <adamovskij00@mail.ru>
2021-11-10 18:46:47 +0300

    add Fun with ES6 Classes #3 - Cuboids, Cubes and Getters

--- main.js
+++ main.js
@@ -1095,3 +1095,27 @@ class Dog extends Animal {
         return `Hello ${this.master}`;
     }
 }
+
+// Fun with ES6 Classes #3 - Cuboids, Cubes and Getters
+
+class Cuboid {
+    constructor(length, width, height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    get volume() {
+        return this.length * this.width * this.height;
+    }
+
+    get surfaceArea() {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+}
+
+class Cube extends Cuboid {
+    constructor(length) {
+        super(length, length, length)
+    }
+}
