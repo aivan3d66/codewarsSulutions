57a24544c00f1b1712509a79f80cf5d052b67827
Ivan <adamovskij00@mail.ru>
2021-11-10 18:35:14 +0300

    add Basic Mathematical Operations

--- main.js
+++ main.js
@@ -794,3 +794,25 @@ function sum(num) {
 function opposite(number) {
     return number = -number;
 }
+
+// Basic Mathematical Operations
+
+function basicOp(operation, value1, value2)
+{
+    let result;
+    switch (operation) {
+        case '+':
+            result = value1 + value2;
+            break;
+        case '-':
+            result = value1 - value2;
+            break;
+        case '*':
+            result = value1 * value2;
+            break;
+        case '/':
+            result = value1 / value2;
+            break;
+    }
+    return result;
+}
