3d94ae8971b88154134ffff3d11a9b45546a3487
Ivan <adamovskij00@mail.ru>
2021-11-10 18:44:13 +0300

    add Extract Nested Object Reference

--- main.js
+++ main.js
@@ -1006,3 +1006,16 @@ function isSantaClausable(obj) {
     if (typeof obj["goDownTheChimney"] != "function") return false;
     return true;
 }
+
+// Extract Nested Object Reference
+// return the nested property value if it exists,
+// otherwise return undefined
+Object.prototype.hash = function(string) {
+    let obj = this;
+    let arr = string.split(".").forEach(function(elem) {
+        if (obj) {
+            obj = obj[elem];
+        }
+    });
+    return obj;
+}
