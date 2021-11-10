bdcbb9da20343e8b0be53deb721072a2a17a56a3
Ivan <adamovskij00@mail.ru>
2021-11-10 09:00:28 +0300

    Slice and Splice

--- main.js
+++ main.js
@@ -672,3 +672,12 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+//  Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+    let combinedArrays = arr2.slice()
+    combinedArrays.splice(n, 0, ...arr1)
+    return combinedArrays;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
