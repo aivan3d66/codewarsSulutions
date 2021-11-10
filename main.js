eaa5a4b5ed04195830a41279ce90200b20f39c73
Ivan <adamovskij00@mail.ru>
2021-11-10 18:47:16 +0300

    add New with apply

--- main.js
+++ main.js
@@ -1119,3 +1119,10 @@ class Cube extends Cuboid {
         super(length, length, length)
     }
 }
+
+// new with apply
+function construct(Class) {
+    let obj = Object.create(Class.prototype);
+    Class.apply(obj, Array.prototype.slice.call(arguments, 1));
+    return obj;
+}
