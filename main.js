71b5f5ccf415a9772faeeafcc5c6172c193fe3bb
Ivan <adamovskij00@mail.ru>
2021-11-10 18:44:38 +0300

    add Array Helpers

--- main.js
+++ main.js
@@ -1019,3 +1019,30 @@ Object.prototype.hash = function(string) {
     });
     return obj;
 }
+
+// Array Helpers
+
+Array.prototype.square = function() {
+    return this.map(el=>el**2)
+};
+
+Array.prototype.cube = function() {
+    return this.map(el=>el**3)
+};
+
+Array.prototype.average = function() {
+    if (!this.length) return NaN;
+    return this.reduce((acc, el) => acc + el, 0) / this.length;
+};
+
+Array.prototype.sum = function() {
+    return this.reduce((acc, el) => acc + el, 0);
+};
+
+Array.prototype.even = function() {
+    return this.filter(el => !(el%2));
+};
+
+Array.prototype.odd = function() {
+    return this.filter(el => !!(el%2))
+};
