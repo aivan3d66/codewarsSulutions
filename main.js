6288a0b796d555bc80831cf980a28b48cafc27e1
Ivan <adamovskij00@mail.ru>
2021-11-10 18:36:13 +0300

    add Transportation on vacation

--- main.js
+++ main.js
@@ -822,3 +822,12 @@ function basicOp(operation, value1, value2)
 function printArray(array){
     return array.join(',');
 }
+
+// Transportation on vacation
+
+function rentalCarCost(d) {
+    let day = d >= 3 && d < 7 ? 20 : 0;
+    let week = d >= 7 ? 50 : 0;
+
+    return (d * 40) - day - week;
+}
