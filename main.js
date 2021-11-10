b33a8212ef1c417d900530bb11398f8f69bef943
Ivan <adamovskij00@mail.ru>
2021-11-10 18:43:15 +0300

    add Calculating with Functions

--- main.js
+++ main.js
@@ -933,3 +933,68 @@ function createMessage(str) {
         return createMessage(str+' '+str1)
     }
 }
+
+// Calculating with Functions
+function zero(func) {
+    return func ? func(0) : 0;
+}
+
+function one(func) {
+    return func ? func(1) : 1;
+}
+
+function two(func) {
+    return func ? func(2) : 2;
+}
+
+function three(func) {
+    return func ? func(3) : 3;
+}
+
+function four(func) {
+    return func ? func(4) : 4;
+}
+
+function five(func) {
+    return func ? func(5) : 5;
+}
+
+function six(func) {
+    return func ? func(6) : 6;
+}
+
+function seven(func) {
+    return func ? func(7) : 7;
+}
+
+function eight(func) {
+    return func ? func(8) : 8;
+}
+
+function nine(func) {
+    return func ? func(9) : 9;
+}
+
+function plus(a2) {
+    return function (a1) {
+        return a1 + a2;
+    }
+}
+
+function minus(a2) {
+    return function (a1) {
+        return a1 - a2;
+    }
+}
+
+function times(a2) {
+    return function (a1) {
+        return a1 * a2;
+    }
+}
+
+function dividedBy(a2) {
+    return function (a1) {
+        return Math.floor(a1 / a2)
+    }
+}
