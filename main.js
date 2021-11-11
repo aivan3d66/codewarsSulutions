ce0c2609f2e387ae7852f274a8eb7cbe22606860
Ivan <adamovskij00@mail.ru>
2021-11-11 11:14:51 +0300

    add Only change code below this line

--- main.js
+++ main.js
@@ -1664,3 +1664,18 @@ Dog.prototype.constructor = Dog;
 
 let duck = new Bird();
 let beagle = new Dog();
+
+//  Add Methods After Inheritance
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function() {
+    console.log("Woof!");
+}
+
+let beagle = new Dog();
