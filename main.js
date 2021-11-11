ae77a47b99eaf04e30cdf75fd92c311242008c5f
Ivan <adamovskij00@mail.ru>
2021-11-11 11:11:22 +0300

    add Reset an Inherited Constructor Property

--- main.js
+++ main.js
@@ -1649,3 +1649,18 @@ Animal.prototype = {
 function Dog() { }
 Dog.prototype = Object.create(Animal.prototype);
 let beagle = new Dog();
+
+//  Reset an Inherited Constructor Property
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
