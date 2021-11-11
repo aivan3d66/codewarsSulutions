ac5e9cc2daa254af26b10b3cbb0f11057666ac04
Ivan <adamovskij00@mail.ru>
2021-11-11 10:53:18 +0300

    add Understand the Prototype Chain

--- main.js
+++ main.js
@@ -1586,3 +1586,13 @@ function Dog(name) {
 
 let beagle = new Dog("Snoopy");
 Dog.prototype.isPrototypeOf(beagle);
+
+//  Understand the Prototype Chain
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);
+Object.prototype.isPrototypeOf(Dog.prototype);
