30ac6c641d0396f4682c8b97a9ba19ea5481eed2
Ivan <adamovskij00@mail.ru>
2021-11-11 11:36:12 +0300

    add Use an IIFE to Create a Module

--- main.js
+++ main.js
@@ -1732,4 +1732,29 @@ duck.getWeight();
     console.log("A cozy nest is ready");
 })();
 
+//  Use an IIFE to Create a Module
+let isCuteMixin = function(obj) {
+    obj.isCute = function() {
+        return true;
+    };
+};
+let singMixin = function(obj) {
+    obj.sing = function() {
+        console.log("Singing to an awesome tune");
+    };
+};
 
+let funModule  = (function () {
+    return {
+        isCuteMixin : function(obj) {
+            obj.isCute = function() {
+                return true;
+            };
+        },
+        singMixin: function(obj) {
+            obj.sing = function() {
+                console.log("Singing to an awesome tune");
+            };
+        }
+    }
+})();
