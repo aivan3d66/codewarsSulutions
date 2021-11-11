abb491456f93c6dff286a985589f96ed7c64576b
Ivan <adamovskij00@mail.ru>
2021-11-11 09:14:22 +0300

    add Prevent Object Mutation

--- main.js
+++ main.js
@@ -1459,3 +1459,20 @@ function editInPlace() {
     s[2]=7;
 }
 editInPlace();
+
+// Prevent Object Mutation
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+
+    Object.freeze(MATH_CONSTANTS);
+
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
