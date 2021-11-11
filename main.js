0a5b53f2be79774ad33f3526395a2657ebd9996c
Ivan <adamovskij00@mail.ru>
2021-11-11 10:59:44 +0300

    add Inherit Behaviors from a Supertype

--- main.js
+++ main.js
@@ -1622,3 +1622,16 @@ Animal.prototype = {
         console.log("nom nom nom");
     }
 };
+
+//  Inherit Behaviors from a Supertype
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+    }
+};
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
