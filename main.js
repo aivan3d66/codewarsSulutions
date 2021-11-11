3f3937219e3a4b64ceb2b85d6683f0c6e4751519
Ivan <adamovskij00@mail.ru>
2021-11-11 09:14:56 +0300

    add Write Arrow Functions with Parameters

--- main.js
+++ main.js
@@ -1479,3 +1479,7 @@ const PI = freezeObj();
 
 // Use Arrow Functions to Write Concise Anonymous Functions
 const magic = () => new Date();
+
+//  Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
