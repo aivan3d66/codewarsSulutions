b2a4a9b715baab270d7b4c08a4cdc37e26bf95d6
Ivan <adamovskij00@mail.ru>
2021-11-11 11:04:06 +0300

    add Set the Child's Prototype to an Instance of the Parent

--- main.js
+++ main.js
@@ -1635,3 +1635,17 @@ Animal.prototype = {
 
 let duck = Object.create(Animal.prototype);
 let beagle = Object.create(Animal.prototype);
+
+//  Set the Child's Prototype to an Instance of the Parent
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+    }
+};
+
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+let beagle = new Dog();
