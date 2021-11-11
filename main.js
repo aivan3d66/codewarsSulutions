f4b7f651dff89c72be1103614e7e72743c953adc
Ivan <adamovskij00@mail.ru>
2021-11-11 09:25:51 +0300

    add  Use Prototype Properties to Reduce Duplicate Code

--- main.js
+++ main.js
@@ -1508,3 +1508,11 @@ for (let property in canary) {
         ownProps.push(property);
     }
 }
+
+//  Use Prototype Properties to Reduce Duplicate Code
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+let beagle = new Dog("Snoopy");
