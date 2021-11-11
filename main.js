81fe67b5326dd532ae693185ccfe8ebda330dcbc
Ivan <adamovskij00@mail.ru>
2021-11-11 09:10:17 +0300

    add Best travel

--- main.js
+++ main.js
@@ -1312,3 +1312,25 @@ var isPP = function(n){
     }
     return null;
 }
+
+// Best travel
+function chooseBestSum(t, k, ls) {
+    var biggestCount = 0;
+    var recurseTowns = function(townsSoFar, lastIndex) {
+        townsSoFar = townsSoFar || [];
+
+        if (townsSoFar.length === k) {
+            var sumDistance = townsSoFar.reduce((a,b)=>a+b);
+            if (sumDistance <= t && sumDistance > biggestCount) {
+                biggestCount = sumDistance;
+            }
+            return;
+        }
+
+        for (var i = lastIndex + 1 || 0; i < ls.length; i++) {
+            recurseTowns(townsSoFar.concat(ls[i]), i);
+        }
+    }
+    recurseTowns();
+    return biggestCount || null;
+}
