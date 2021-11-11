1f055ce5d0c2144663540b9944a47a757b9711e6
Ivan <adamovskij00@mail.ru>
2021-11-11 09:13:35 +0300

    add Mutate an Array Declared with const

--- main.js
+++ main.js
@@ -1450,3 +1450,12 @@ function checkScope() {
 }
 
 checkScope();
+
+// Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+    s[0] = 2;
+    s[1]=5;
+    s[2]=7;
+}
+editInPlace();
