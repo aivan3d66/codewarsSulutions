099199839658a76438ddc7ee53a8fc5534bc13fc
Ivan <adamovskij00@mail.ru>
2021-11-11 10:57:39 +0300

    add Use Inheritance So You Don't Repeat Yourself

--- main.js
+++ main.js
@@ -1596,3 +1596,29 @@ let beagle = new Dog("Snoopy");
 
 Dog.prototype.isPrototypeOf(beagle);
 Object.prototype.isPrototypeOf(Dog.prototype);
+
+//  Use Inheritance So You Don't Repeat Yourself
+function Cat(name) {
+    this.name = name;
+}
+
+Cat.prototype = {
+    constructor: Cat,
+};
+
+function Bear(name) {
+    this.name = name;
+}
+
+Bear.prototype = {
+    constructor: Bear,
+};
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+    }
+};
