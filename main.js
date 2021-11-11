d8760eba94989f6acc7182d6ca254a29aa3e4ab0
Ivan <adamovskij00@mail.ru>
2021-11-11 11:17:36 +0300

    add Only change code below this line

--- main.js
+++ main.js
@@ -1679,3 +1679,20 @@ Dog.prototype.bark = function() {
 }
 
 let beagle = new Dog();
+
+//  Override Inherited Methods
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird.";
+};
+
+let penguin = new Penguin();
+console.log(penguin.fly());
