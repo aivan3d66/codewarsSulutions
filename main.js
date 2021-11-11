e589f9ab21a35a772e570c151214213a482ab956
Ivan <adamovskij00@mail.ru>
2021-11-11 09:36:54 +0300

    add Remember to Set the Constructor Property when Changing the Prototype

--- main.js
+++ main.js
@@ -1562,3 +1562,19 @@ Dog.prototype = {
         console.log("Say my name " + this.name)
     }
 };
+
+//  Remember to Set the Constructor Property when Changing the Prototype
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function() {
+        console.log("nom nom nom");
+    },
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+};
