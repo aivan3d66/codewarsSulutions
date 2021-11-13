94344a78b47c0ea18806f068b9fe146bfb93498e
Ivan <adamovskij00@mail.ru>
2021-11-13 23:37:03 +0300

    add Use getters and setters to Control Access to an Object

--- main.js
+++ main.js
@@ -1888,3 +1888,21 @@ import {uppercaseString, lowercaseString} from './string_functions.js';
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+//  Use getters and setters to Control Access to an Object
+class Thermostat {
+    constructor(fahrenheit) {
+        this._tempInCelsius = 5/9 * (fahrenheit - 32);
+    }
+    get temperature(){
+        return this._tempInCelsius;
+    }
+    set temperature(newTemp){
+        this._tempInCelsius = newTemp;
+    }
+}
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
