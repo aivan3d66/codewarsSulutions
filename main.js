5c0c53a394c1cb19ea4ec4fba93625880f09fb43
Ivan <adamovskij00@mail.ru>
2021-11-18 11:13:17 +0300

    add ternar oper

--- main.js
+++ main.js
@@ -2007,3 +2007,8 @@ function NameMe(first, last) {
         lastName: this.lastName
     };
 }
+
+//  if..else and ternary operator
+function saleHotdogs(n) {
+    return (n < 5) ? n * 100 : (n >= 5 && n < 10) ? n * 95 : n * 90;
+}
