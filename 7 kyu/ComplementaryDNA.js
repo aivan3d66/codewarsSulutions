3dbe7f5b590855bd84568041eab28eb694105dc9
Ivan <adamovskij00@mail.ru>
2022-02-26 16:43:24 +0300

    add dnaStrand func

--- 7 kyu/ComplementaryDNA.js
+++ 7 kyu/ComplementaryDNA.js
@@ -7,3 +7,26 @@
 //
 // "ATTGC" --> "TAACG"
 // "GTAT" --> "CATA"
+
+const dnaStrand = (dna) => {
+  let target = dna.split(/\B/g);
+  for (let i = 0; i < target.length; i++) {
+    switch (target[i]) {
+      case 'A':
+        target[i] = 'T';
+        continue;
+      case 'T':
+        target[i] = 'A';
+        continue;
+      case 'C':
+        target[i] = 'G';
+        continue;
+      case 'G':
+        target[i] = 'C';
+        continue;
+    }
+  }
+  return target.join("");
+}
+
+console.log(dnaStrand("ATTGC"))
