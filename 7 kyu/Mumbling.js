2939e266f6d78c4ed4eb8af3dfc798308c4c630f
Ivan <adamovskij00@mail.ru>
2022-03-02 21:38:35 +0300

    add Mumbling solution

--- 7 kyu/Mumbling.js
+++ 7 kyu/Mumbling.js
@@ -1,2 +1,10 @@
 // This time no story, no theory.
 // The examples below show you how to write function accum:
+
+const accum = (s) => {
+  return s.split('').map((e, i) => {
+    return e.toUpperCase() + e.toLowerCase().repeat(i)
+  }).join('-');
+}
+
+console.log(accum("ZpglnRxqenU"))
