b1189df2db0331d7178aa44fcd33b3093da9f62e
Ivan <adamovskij00@mail.ru>
2022-03-23 15:01:52 +0300

    add "Range Extraction" kata solution

--- 4 kyu/rangeExtraction.js
+++ 4 kyu/rangeExtraction.js
@@ -11,3 +11,30 @@
 //
 // solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
 // // returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+function solution(list) {
+    let goal = "",
+        tmp = "";
+    for (let i = 0; i < list.length; i++) {
+        if (divideTest(list[i], list[i + 1]) !== 1) {
+            goal += list[i] + ",";
+            tmp = "";
+        } else {
+            let j = i,
+                counter = 0;
+            for (; j < list.length; j++) {
+                if (divideTest(list[j], list[j + 1]) === 1) {
+                    tmp = list[j + 1];
+                    counter++;
+                } else {
+                    goal += list[i];
+                    i = j;
+                    break;
+                }
+            }
+            goal += counter > 1 ? "-" + tmp + "," : "," + tmp + ",";
+        }
+    }
+    return goal.slice(0, -1);
+}
+const divideTest = (a, b) => a > b ? Math.abs(a - b) : Math.abs(b - a);
