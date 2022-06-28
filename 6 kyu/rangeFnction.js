9b57cb0f2b9dbb8a8e36f6ee0867b9ac13b6b763
Ivan <adamovskij00@mail.ru>
2022-06-28 17:11:51 +0300

    add "Range function" kata solution

--- 6 kyu/rangeFnction.js
+++ 6 kyu/rangeFnction.js
@@ -6,3 +6,16 @@
 // range(5) --> 1,2,3,4,5
 // range(3, 7) --> 3,4,5,6,7
 // range(2, 3, 15) --> 2,5,8,11,14
+
+let range = function (start, step, stop) {
+  let arr = [];
+  if (arguments.length === 1)
+    for (let i = 1; i <= start; i++) {
+      arr.push(i);
+    }
+  else if (arguments.length === 2)
+    for (let i = start; i <= step; i++) arr.push(i);
+  else if (arguments.length === 3)
+    for (; start <= stop; start += step) arr.push(start);
+  return arr;
+};
