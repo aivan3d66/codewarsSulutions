6a240e00a3e1892b919740e7ee989e9e17f43e8e
Ivan <adamovskij00@mail.ru>
2022-06-29 14:42:50 +0300

    add "Linked Lists - Insert Nth Node" kata solution

--- 6 kyu/linkedListNthNode.js
+++ 6 kyu/linkedListNthNode.js
@@ -12,3 +12,51 @@
 // You must throw/raise an exception (ArgumentOutOfRangeException in C#, InvalidArgumentException in PHP) if the index is too large.
 //
 // The push() and buildOneTwoThree() (build_one_two_three() in PHP) functions do not need to be redefined. The Node class is also preloaded for you in PHP.
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  if (head) {
+    let length = 1;
+    while (head.next) {
+      head = head.next;
+      length++;
+    }
+    return length;
+  }
+  return 0;
+}
+
+function insertNth(head, index, data) {
+  if (index < 0) throw new Exception();
+  if (index > length(head)) throw new Exception();
+  if (!head) {
+    head = index === 0 ? new Node(data) : new Node(null);
+    return head;
+  }
+
+  let node = new Node(data),
+    curNode = head,
+    count = 0;
+  if (index === 0) {
+    node.next = head;
+    return node;
+  }
+
+  while (true) {
+    if (count + 1 === index) {
+      node.next = curNode.next;
+      curNode.next = node;
+      return head;
+    }
+    if (!curNode.next) {
+      curNode.next = node;
+      return head;
+    }
+    curNode = curNode.next;
+    count++;
+  }
+}
