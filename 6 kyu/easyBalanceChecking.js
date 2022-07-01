771787deea6cd210018b1232cdba216cdb126f43
Ivan <adamovskij00@mail.ru>
2022-07-01 17:04:18 +0300

    add "Find X" kata solution

--- 6 kyu/easyBalanceChecking.js
+++ 6 kyu/easyBalanceChecking.js
@@ -28,3 +28,43 @@
 //   Round to 2 decimals your calculated results (Elm: without traling 0)
 // The line separator of results may depend on the language \n or \r\n. See examples in the "Sample tests".
 //   R language: Don't use R's base function "mean()" that could give results slightly different from expected ones.
+
+function balance(book) {
+  let arrBook = bookToArray(book);
+
+  arrBook[0][0] = numberFormat(arrBook[0][0], 2);
+  let balance = parseFloat(arrBook[0][0]),
+    avg = 0;
+
+  for (let i = 1; i < arrBook.length; i++) {
+    arrBook[i][1] = arrBook[i][1].replace(/\W+/g, '');
+    arrBook[i][2] = numberFormat(arrBook[i][2], 2);
+    balance -= +arrBook[i][2];
+    avg += +arrBook[i][2];
+    arrBook[i].push('Balance ' + balance.toFixed(2));
+  }
+
+  arrBook.push(
+    [`Total expense  ${(+arrBook[0][0] - balance).toFixed(2)}`], [`Average expense  ${(avg / (arrBook.length - 1)).toFixed(2)}`]
+  );
+  arrBook[0][0] = 'Original Balance: ' + arrBook[0][0];
+  console.log(arrBook);
+
+  return bookToString(arrBook);
+
+  function bookToString(array) {
+    return array.map(e => e.join(' ')).join('\r\n');
+  }
+
+  function bookToArray(book) {
+    return book.split('\n')
+      .map(e => e.split(' '))
+      .filter(e => e.toString() !== '');
+  }
+
+  function numberFormat(str, cur = 2) {
+    str = str.replace(/[^(\d+\.\d+)]/g, '');
+    str = (+str).toFixed(cur);
+    return str;
+  }
+}
