ca667bdfc297aaf86dddf81e5df78ed34627f005
Ivan <adamovskij00@mail.ru>
2022-07-01 17:01:08 +0300

    add "Find X" kata solution

--- 6 kyu/findTheOdd.js
+++ 6 kyu/findTheOdd.js
@@ -8,3 +8,5 @@
 // [1,1,2] should return 2, because it occurs 1 time (which is odd).
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
