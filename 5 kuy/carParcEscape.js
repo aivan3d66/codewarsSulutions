a3c28c6a42c01df80d2fe1d7217778376d32d69e
Ivan <adamovskij00@mail.ru>
2022-07-04 15:53:56 +0300

    add "Car park escape" kata solution

--- 5 kuy/carParcEscape.js
+++ 5 kuy/carParcEscape.js
@@ -41,3 +41,47 @@
 //   - To escape you have to move Right 4 places => "R4"
 // Result
 // result = ["L4", "D1", "R4"]
+
+function escape(carpark) {
+  let result = [],
+    pos = findStart();
+
+  for (let i = 0; i < carpark[pos[0]].length;) {
+    if (pos[0] === carpark.length - 1 && pos[1] === carpark[carpark.length - 1].length - 1) {
+      break;
+    }
+    if (pos[0] === carpark.length - 1 && pos[1] !== carpark[carpark.length - 1].length) {
+      result.push("R" + (carpark[carpark.length - 1].length - pos[1] - 1));
+      break;
+    }
+    if (carpark[pos[0]][i] === 1) {
+      if (i > pos[1]) result.push("R" + (i - pos[1]));
+      else result.push("L" + (pos[1] - i));
+
+      let deep = 1;
+      if (carpark[pos[0] + 1][i] === 1) {
+        for (let j = pos[0] + 1; j < carpark.length; j++)
+          if (carpark[j][i] === 1) deep++;
+          else break;
+      }
+
+      result.push("D" + deep);
+
+      pos[0] += deep;
+      pos[1] = i;
+      i = 0;
+      continue;
+    }
+    i++;
+  }
+
+  return result;
+
+  function findStart() {
+    for (let i = 0; i < carpark.length; i++) {
+      for (let z = 0; z < carpark[i].length; z++) {
+        if (carpark[i][z] === 2) return [i, z];
+      }
+    }
+  }
+}
