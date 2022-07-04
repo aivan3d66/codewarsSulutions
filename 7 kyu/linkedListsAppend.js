ac24cb53b52fa459b409736cfc9f8d82e334d4b8
Ivan <adamovskij00@mail.ru>
2022-07-04 15:44:36 +0300

    add "Linked Lists - Append" kata solution

--- 7 kyu/linkedListsAppend.js
+++ 7 kyu/linkedListsAppend.js
@@ -8,3 +8,23 @@
 // append(listA, listB) === 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
 // If both listA and listB are null/NULL/None/nil, return null/NULL/None/nil.
 // If one list is null/NULL/None/nil and the other is not, simply return the non-null/NULL/None/nil list.
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function append(listA, listB) {
+  if (!listA && !listB) return null;
+  if (!listA) return listB;
+  else if (!listB) return listA;
+
+  let head = listA;
+  while (listA) {
+    if (!listA.next) {
+      listA.next = listB;
+      return head;
+    }
+    listA = listA.next;
+  }
+}
