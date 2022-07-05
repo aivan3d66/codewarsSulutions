651db04882d00eafdb6f85b3fd0d1e6e9762e22c
Ivan <adamovskij00@mail.ru>
2022-07-05 16:21:16 +0300

    add "Land perimeter" kata solution

--- 5 kuy/landPerimeter.js
+++ 5 kuy/landPerimeter.js
@@ -19,3 +19,21 @@
 //   should return: "Total land perimeter: 18"
 //
 // Good luck!
+
+function landPerimeter(arr) {
+  let perimiter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let z = 0; z < arr[i].length; z++) {
+      if (arr[i][z] === 'X') {
+        perimiter += 4;
+        if (arr[i][z + 1] === 'X') perimiter--;
+        if (arr[i][z - 1] === 'X') perimiter--;
+        if (typeof arr[i - 1] !== 'undefined')
+          if (arr[i - 1][z] === 'X') perimiter--;
+        if (typeof arr[i + 1] !== 'undefined')
+          if (arr[i + 1][z] === 'X') perimiter--;
+      }
+    }
+  }
+  return "Total land perimeter: " + perimiter;
+}
