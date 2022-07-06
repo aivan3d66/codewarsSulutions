0e0ecd3fc034935651c9309e1f727f8d2c27e52a
Ivan <adamovskij00@mail.ru>
2022-07-06 13:36:17 +0300

    add "Last digit of a large number" kata instruction

---
+++ 5 kuy/lastDigitLargeNumber.js
@@ -0,0 +1,7 @@
+// Define a function that takes in two non-negative integers a and b and returns the last decimal digit of ab^ba
+// Note that aaa and bbb may be very large!
+//
+// // lastDigit("4", "1")            // returns 4
+// // lastDigit("4", "2")            // returns 6
+// // lastDigit("9", "7")            // returns 9
+// // lastDigit("10","10000000000")  // returns 0
