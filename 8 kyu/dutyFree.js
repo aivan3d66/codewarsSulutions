c9c2d86a9ac3ee0aac8b1683e737f74c8434302c
Ivan <adamovskij00@mail.ru>
2022-07-07 16:22:27 +0300

    add "Holiday VIII - Duty free" kata solution

--- 8 kyu/dutyFree.js
+++ 8 kyu/dutyFree.js
@@ -4,3 +4,7 @@
 // For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle.
 // If your holiday cost £500, the answer you should return would be 500.
 // All inputs will be integers. Please return an integer. Round down.
+
+function dutyFree(normPrice, discount, hol) {
+    return Math.floor(hol / (normPrice * (discount / 100)));
+}
