1ed9dde62fbcd6f3744a3b5fdde46ad77e168a0c
Ivan <adamovskij00@mail.ru>
2022-07-08 13:44:22 +0300

    add "Return Negative" kata solution

--- 8 kyu/returnNegative.js
+++ 8 kyu/returnNegative.js
@@ -8,3 +8,7 @@
 // Notes
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
