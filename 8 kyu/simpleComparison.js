63e3847211518379f6bf112bc750d328ca5a837f
Ivan <adamovskij00@mail.ru>
2022-07-08 13:45:51 +0300

    add "Return Negative" kata solution

--- 8 kyu/simpleComparison.js
+++ 8 kyu/simpleComparison.js
@@ -2,3 +2,9 @@
 // Return true if they are the same character (regardless of their different data types) and return false if they are not.
 // To make this challange harder and to promp the challenger to read up about coercion
 // I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+
+function add(a, b) {
+  if (typeof a === 'number') return a === +b;
+  else if (typeof b === 'number') return +a === b;
+  else return a === b;
+}
