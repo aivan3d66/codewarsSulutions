2db46f0639b5dfa936b273b4be3eafbe24d749b2
Ivan <adamovskij00@mail.ru>
2022-07-11 15:27:42 +0300

    add "Don't rely on luck HARDCORE" kata instruction

---
+++ 3 kyu/dontRelyHardcore.js
@@ -0,0 +1,9 @@
+//   The test fixture I use for this kata is incredibly harder than this kata: http://www.codewars.com/kata/dont-rely-on-luck/train/javascript
+//   You should be able to finish that kata before this.
+//   This test fixture will compare your guess to a random number generated in Javascript/CoffeeScript by:
+//   Math.floor(Math.random() * 100000 + 1) You can pass only by relying on skill - because our tests will be repeated for multiple times.
+//   And note that in HARDCORE mode: there are strict anti-cheat measures!! PREPARE to hack into the internals of javascript.
+//   Also note that there are cheating methods that can solve all katas,
+//   but please do not use them, they will be detected by the Codewars system (by testing if tests that should fail actually pass)
+//   and you will lose your points. Please use a method that only works for this particular kata.
+//   Finished this one? You are up for a harder challenge: http://www.codewars.com/kata/55041210decf057109000277
