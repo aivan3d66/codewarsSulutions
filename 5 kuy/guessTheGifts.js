9012231dd0c924c99929da3a546b9aaead314362
Ivan <adamovskij00@mail.ru>
2022-07-14 10:37:45 +0300

    add "Guess The Gifts!" kata solution

--- 5 kuy/guessTheGifts.js
+++ 5 kuy/guessTheGifts.js
@@ -29,3 +29,16 @@
 // ];
 //
 // guessGifts(wishlist, presents); // must return ["Toy Car", "Mini Puzzle"]
+
+function guessGifts(wishlist, presents) {
+  return wishlist
+    .filter(whish =>
+      presents.some(
+        present =>
+          present.size === whish.size &&
+          present.clatters === whish.clatters &&
+          present.weight === whish.weight
+      )
+    )
+    .map(whish => whish.name)
+}
