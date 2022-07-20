26fdda0b4daeca6909bc38e7d9a38e0d41c8170d
Ivan <adamovskij00@mail.ru>
2022-07-20 13:39:05 +0300

    add "Weight for weight" kata solution

--- 5 kuy/weightForWeight.js
+++ 5 kuy/weightForWeight.js
@@ -19,3 +19,18 @@
 //   Notes
 // it may happen that the input string have leading, trailing whitespaces and more than a unique whitespace between two consecutive numbers
 // For C: The result is freed.
+
+function orderWeight(strng) {
+  return strng
+    .split(' ')
+    .map(number => {
+      const weight = number.split('').reduce((a, b) => Number(a) + Number(b), 0)
+      return { num: number, weight }
+    })
+    .sort(
+      (a, b) =>
+        a.weight === b.weight ? a.num.localeCompare(b.num) : a.weight - b.weight
+    )
+    .map(({ num }) => num)
+    .join(' ')
+}
