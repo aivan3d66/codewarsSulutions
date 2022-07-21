b0623908ae6e7dc87d073ccca938674ba933711d
Ivan <adamovskij00@mail.ru>
2022-07-21 17:10:08 +0300

    add "Argument Mapper?" kata solution

--- 5 kuy/argumentMapper.js
+++ 5 kuy/argumentMapper.js
@@ -14,3 +14,17 @@
 // Invalid inputs, e.g. non-function objects, or wrong number of arguments, are not considered.
 //
 //   Hajime!
+
+const createArgumentMap = (func, ...values) => {
+  const funcStr = func.toString();
+  const start = funcStr.indexOf('(') + 1;
+  const end = funcStr.indexOf(')');
+  const args = funcStr.slice(start, end).split(',');
+
+  if (!values.length) return [];
+
+  return args.reduce((obj, arg, ind) => {
+    obj[arg] = values[ind];
+    return obj;
+  }, {});
+};
