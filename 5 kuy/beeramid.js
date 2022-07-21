7f8f4bcc818bff1925515814fddbcf293b3bd19a
Ivan <adamovskij00@mail.ru>
2022-07-21 17:11:44 +0300

    add "Beeramid" kata solution

--- 5 kuy/beeramid.js
+++ 5 kuy/beeramid.js
@@ -12,3 +12,13 @@
 //
 //   beeramid(1500, 2); // should === 12
 // beeramid(5000, 3); // should === 16
+
+var beeramid = function(bonus, price) {
+  let n = 0
+  let i = 0;
+  while(true){
+    i+=1
+    if (bonus-i*i*price<0) return i-1
+    bonus-=i*i*price
+  }
+}
