c6901c2a727b98f1e8e6d03118e31d14f05d1616
Ivan <adamovskij00@mail.ru>
2022-07-22 16:35:29 +0300

    add "Binary Tree Serpentine Traversal" kata solution

--- 5 kuy/binaryTreeSerpentineTraversal.js
+++ 5 kuy/binaryTreeSerpentineTraversal.js
@@ -13,3 +13,20 @@
 // / \  / \
 // D E  F G
 // Should result in ["A", "C", "B", "D", "E", "F", "G"]
+
+// Make sure you are counting the first level (root) as
+// being read from left to right.
+function serpentineTree(node)
+{
+  const left = {}
+  dfs(node,left)
+  return Object.values(left).reduce((arr,val,i)=>arr.concat(i%2?val.reverse():val),[])
+}
+
+function dfs(node,obj,d=0){
+  if (!node) return;
+  if (!obj[d]) obj[d]=[]
+  obj[d].push(node.data)
+  dfs(node.left,obj,d+1)
+  dfs(node.right,obj,d+1)
+}
