7b478cda8d9c95a3a02d87e9fcd972fee1dae752
Ivan <adamovskij00@mail.ru>
2022-07-22 16:38:29 +0300

    add "Binary Tree Traversal" kata solution

--- 5 kuy/binaryTreeTraversal.js
+++ 5 kuy/binaryTreeTraversal.js
@@ -42,3 +42,42 @@
 // preOrder(a) should return ["A", "B", "C", "D"]
 // inOrder(a) should return ["B", "A", "D", "C"]
 // postOrder(a) should return ["B", "D", "C", "A"]
+
+function preOrder(node)
+{
+  const arr = []
+  t(node,arr)
+  return arr
+  function t(node,arr){
+    if (!node) return;
+    arr.push(node.data)
+    t(node.left,arr)
+    t(node.right,arr)
+  }
+}
+
+function inOrder(node)
+{
+  const arr = []
+  t(node,arr)
+  return arr
+  function t(node,arr){
+    if (!node) return;
+    t(node.left,arr)
+    arr.push(node.data)
+    t(node.right,arr)
+  }
+}
+
+function postOrder(node)
+{
+  const arr = []
+  t(node,arr)
+  return arr
+  function t(node,arr){
+    if (!node) return;
+    t(node.left,arr)
+    t(node.right,arr)
+    arr.push(node.data)
+  }
+}
