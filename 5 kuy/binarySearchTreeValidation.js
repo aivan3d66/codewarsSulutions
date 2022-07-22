f5a1605dbd671dee1a8316e8ddcb6fe6cde3499e
Ivan <adamovskij00@mail.ru>
2022-07-22 16:41:20 +0300

    add "Binary search tree validation" kata solution

--- 5 kuy/binarySearchTreeValidation.js
+++ 5 kuy/binarySearchTreeValidation.js
@@ -37,3 +37,25 @@
 // There are several different approaches you can take to solve this kata. If you're not as comfortable with recursion I'd recommend practicing that.
 //
 // Note: no test case tree will contain duplicate numbers.
+
+class Node {
+  constructor(value, left = null, right = null){
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const isBST = node => {
+  const arr1 = [];
+  t(node,arr1);
+  return JSON.stringify(arr1)===JSON.stringify(arr1.slice().sort((a,b)=>a-b))||
+    JSON.stringify(arr1)===JSON.stringify(arr1.slice().sort((a,b)=>b-a))
+};
+
+function t(root,arr){
+  if(!root) return;
+  t(root.left,arr)
+  arr.push(root.value)
+  t(root.right,arr)
+}
