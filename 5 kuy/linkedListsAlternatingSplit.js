e203f8e5acff58f0b91f277e958c24912b22a355
Ivan <adamovskij00@mail.ru>
2022-08-15 11:16:56 +0300

    add "Linked Lists - Alternating Split" kata solution

--- 5 kuy/linkedListsAlternatingSplit.js
+++ 5 kuy/linkedListsAlternatingSplit.js
@@ -11,3 +11,24 @@
 // A Context object containing the two mutated lists should be returned by AlternatingSplit().
 //
 //   If the passed in head node is null/None/nil or a single node, throw an error.
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function Context(first, second) {
+  this.first = first;
+  this.second = second;
+}
+
+function alternatingSplit(head) {
+  if (!head || !head.next) throw new Error("invalid arguments");
+  return new Context(split(head), split(head.next));
+}
+
+function split(head) {
+  const list = new Node(head.data);
+  if (head.next && head.next.next) list.next = split(head.next.next);
+  return list;
+}
