be424265941157e932805ab289812984bd82c594
Ivan <adamovskij00@mail.ru>
2022-08-16 10:54:05 +0300

    add "Merged String Checker" kata solution

--- 5 kuy/mergedStringChecker.js
+++ 5 kuy/mergedStringChecker.js
@@ -10,3 +10,10 @@
 // s:  c o d e w a r s   = codewars
 // part1:  c   d   w         = cdw
 // part2:    o   e   a r s   = oears
+
+function isMerge(s, part1, part2) {
+    return !s
+        ? !(part1 || part2)
+        : (s[0] == part1[0] && isMerge(s.slice(1), part1.slice(1), part2)) ||
+        (s[0] == part2[0] && isMerge(s.slice(1), part1, part2.slice(1)));
+}
