8082c124e2b76737a1468c35ecf702d953fd163f
Ivan <adamovskij00@mail.ru>
2022-08-16 11:05:18 +0300

    add "My smallest code interpreter" kata solution

--- 5 kuy/mySmallestCodeInterpreter.js
+++ 5 kuy/mySmallestCodeInterpreter.js
@@ -24,3 +24,51 @@
 // Error-handling, e.g. unmatched square brackets and/or memory pointer going past the leftmost cell
 // is not required in this Kata. If you see test cases that require you to perform error-handling then
 // please open an Issue in the Discourse for this Kata (don't forget to state which programming language you are attempting this Kata in).
+
+function brainLuck(code, input) {
+    code = code.split("").filter(c => "><+-.,[]".includes(c));
+    let output = "",
+        ptr = 0,
+        brackets = 0,
+        i;
+    const data = [0],
+        map = {
+            ">": () => {
+                ptr++;
+                if (ptr >= data.length) data.push(0);
+            },
+            "<": () => ptr--,
+            "+": () => (data[ptr] = (data[ptr] + 1) % 256),
+            "-": () => (data[ptr] = (data[ptr] - 1) % 256),
+            ",": () => {
+                data[ptr] = input.charCodeAt(0);
+                input = input.slice(1);
+            },
+            ".": () => (output += String.fromCharCode(data[ptr])),
+            "[": () => {
+                if (data[ptr]) return;
+                for (let j = i + 1; j < code.length; j++)
+                    if (code[j] === "[") brackets++;
+                    else if (code[j] === "]")
+                        if (brackets) brackets--;
+                        else {
+                            i = j;
+                            break;
+                        }
+            },
+            "]": () => {
+                if (!data[ptr]) return;
+                for (let j = i - 1; j >= 0; j--)
+                    if (code[j] === "]") brackets++;
+                    else if (code[j] === "[")
+                        if (brackets) brackets--;
+                        else {
+                            i = j;
+                            break;
+                        }
+            }
+        };
+    for (i = 0; i < code.length; i++) map[code[i]]();
+    return output;
+}
+
