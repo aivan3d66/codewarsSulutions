314dfb8732d4b772f2ad913b70c09a241eba6384
Ivan <adamovskij00@mail.ru>
2022-08-21 20:26:24 +0300

    add "Pick peaks" kata solution

--- 5 kuy/pickPeaks.js
+++ 5 kuy/pickPeaks.js
@@ -6,3 +6,18 @@
 // The first and last elements of the array will not be considered as peaks (in the context of a mathematical function, we don't know what is after and before and therefore, we don't know if it is a peak or not).
 // Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] and [1, 2, 2, 2, 2] do not. In case of a plateau-peak, please only return the position and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equivalent in other languages)
 // Have fun!
+
+function pickPeaks(arr) {
+    let result = { pos: [], peaks: [] };
+    for (let i = 1; i < arr.length; i++) {
+        var position;
+        if (arr[i] > arr[i - 1]) {
+            position = i;
+        } else if (arr[i] < arr[i - 1] && position) {
+            result.pos.push(position);
+            result.peaks.push(arr[position]);
+            position = undefined;
+        }
+    }
+    return result;
+}
