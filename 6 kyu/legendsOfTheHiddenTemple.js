acdd917754b54a2ada56877e9ef2ef1c164cc945
Ivan <adamovskij00@mail.ru>
2022-08-27 10:27:19 +0300

    add "Legends of the Hidden Temple" kata solution

--- 6 kyu/legendsOfTheHiddenTemple.js
+++ 6 kyu/legendsOfTheHiddenTemple.js
@@ -28,3 +28,13 @@
 // If n = 1 return 'X\n' and if you're given an even number or invalid input, return '?'.
 //
 //   The output should be a string with no spaces after the final X on each line, but a \n to indicate a new line.
+
+function markSpot(n) {
+  if (typeof n!=='number'||n%2===0||n<1) return '?'
+  let arr = Array.from({length:n},v=>Array.from({length:n*n},v=>' '))
+  for (let i=0,j=0,k=n-1;i<n;i++,j+=2,k--){
+    arr[i][j]='X'
+    arr[k][j]='X'
+  }
+  return arr.map(v=>v.join``.replace(/ *$/,'\n')).join``
+}
