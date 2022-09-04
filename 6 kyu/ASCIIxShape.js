cf5b5aba00657201e4e57e21a90ecd89f07911d4
Ivan <adamovskij00@mail.ru>
2022-09-04 13:35:53 +0300

    add "ASCII Fun #1: X- Shape" kata solution

--- 6 kyu/ASCIIxShape.js
+++ 6 kyu/ASCIIxShape.js
@@ -3,3 +3,12 @@ You will get an odd integer n (>= 3) and your task is to draw an X. Each line is
 
 Use the following characters: ■ □ For Ruby, Crystal and PHP: whitespace and o
  */
+
+function x(n){
+  let arr = Array.from({length:n},x=>Array.from({length:n},x=>'□'))
+  for (let i=0,k=n-1;i<n;i++,k--){
+    arr[i][i]='■'
+    arr[k][i]='■'
+  }
+  return arr.map(v=>v.join``).join`\n`
+}
