52bc54569f815e26be9fc47d1ad6666bfbbcd3d7
Ivan <adamovskij00@mail.ru>
2022-09-04 13:31:49 +0300

    add "A functional deck of cards" kata instruction

---
+++ 6 kyu/aFunctionalDeckOfCards.js
@@ -0,0 +1,20 @@
+/*
+Build a deck of playing cards by generating 52 strings representing cards. There are no jokers.
+
+Each card string will have the format of:
+
+"ace of hearts"
+"ace of spades"
+"ace of diamonds"
+"ace of clubs"
+And will consist of the following ranks:
+
+ace two three four five
+six seven eight nine ten
+jack queen king
+They do not need to be in order.
+
+Additional constraints
+1 line only!
+buildDeck is a variable holding your deck. It is not a function.
+ */
